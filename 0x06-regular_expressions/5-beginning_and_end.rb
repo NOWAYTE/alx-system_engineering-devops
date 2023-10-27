149c53c4390551ce33c07ed6a75ef180ba64f4c0
Daniel kamau nganga <danielkamaunganga3@gmail.com>
2023-10-26 22:51:30 -0600

    Fix script error

--- 0x06-regular_expressions/5-beginning_and_end.rb
+++ 0x06-regular_expressions/5-beginning_and_end.rb
@@ -1,5 +1,3 @@
 #!/usr/bin/env ruby
-# A Script that matches a string that start with h ends with n and can have
-# a single character within 
-
+# A Script that matches a string that start with h ends with n
 puts ARGV[0].scan(/^h.n$/)
