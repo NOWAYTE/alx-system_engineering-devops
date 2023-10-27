645e0455b88e05a3bba970b259effc2c25c6c5cd
Daniel kamau nganga <danielkamaunganga3@gmail.com>
2023-10-27 23:38:34 +0300

    Code updage

--- 0x06-regular_expressions/100-textme.rb
+++ 0x06-regular_expressions/100-textme.rb
@@ -1,14 +1,4 @@
 #!/usr/bin/env ruby
-# A script that outputs [sender], [receiver], [flags]
+# A script that outputs [ sender ] [ receiver ] [ flags ]
 
-
-log_content = File.read(ARGV[0])
-
-parsed_entries = log_content.scan(/\[from:(\+\d+)\] \[to:(\+\d+)\] \[flags:(.*?)\]/)
-
-parsed_entries.each do |entry|
-  sender = entry[0]
-  receiver = entry[1]
-  flags = entry[2]
-  puts "#{sender},#{receiver},#{flags}"
-end
+puts ARGV[0].scan(/\[from:.*?\] \[to:.*?\] \[flags:.*?\]/)
