25393f4ab34959ec68d5c021d24bd4a84c39ba68
Daniel kamau nganga <danielkamaunganga3@gmail.com>
2023-11-26 13:19:19 -0500

    Code update

--- 0x0A-configuration_management/2-execute_a_command.pp
+++ 0x0A-configuration_management/2-execute_a_command.pp
@@ -1,4 +1,5 @@
-exec { 'killmenow':
-  command => 'pkill -f killmenow',
-  path    => '/usr/bin/:/usr/local/bin/:/bin/',
+#Kills a process named killmenow
+
+exec{'pkill -f killmenow':
+path  => '/usr/bin/:/usr/local/bin/:/bin/'
 }
