f167975daadf4afde4b867e7f8c0d06b685fa2be
Daniel kamau nganga <danielkamaunganga3@gmail.com>
2023-11-26 11:46:36 -0500

    Create a file in /tmp

--- 0x0A-configuration_management/0-create_a_file.pp
+++ 0x0A-configuration_management/0-create_a_file.pp
@@ -1,8 +1,9 @@
-file { '/tmp/school' :
-	ensure => file,
-	content => 'I love puppet',
-	mode => '0744',
-	owner => 'www-data',
-	group => 'www-data',
+# create a file
 
+file { '/tmp/school':
+  path    => '/tmp/school',
+  content => 'I love Puppet',
+  mode    => '0744',
+  owner   => 'www-data',
+  group   => 'www-data',
 }
