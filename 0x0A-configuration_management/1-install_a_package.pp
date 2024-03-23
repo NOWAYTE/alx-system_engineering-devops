3e6ace36ecec0c7f34de702bdca6e0f24e48b209
Daniel kamau nganga <danielkamaunganga3@gmail.com>
2024-03-23 04:20:27 +0300

    Updatre

--- 0x0A-configuration_management/1-install_a_package.pp
+++ 0x0A-configuration_management/1-install_a_package.pp
@@ -1,6 +1,6 @@
-# install flask
-package { 'flask':
-  ensure   => '2.1.0',
-  provider => 'pip3',
+exec { 'install_flask':
+  command => '/usr/bin/pip3 install flask==2.1.0',
+  path    => '/usr/bin',
+  creates => '/usr/local/lib/python3.x/dist-packages/flask', # Adjust the path as per your system
 }
 
