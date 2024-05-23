15bde99382cb7613da19cc69a912a7b054cf25e1
Daniel kamau nganga <danielkamaunganga3@gmail.com>
2024-05-23 04:49:58 +0300

    Python script that returns information about his/her list progress

---
+++ 0x15-api/0-gather_data_from_an_API.py
@@ -0,0 +1,24 @@
+#!/usr/bin/python3
+"""
+Uses the JSON placeholder api to query data about an employee
+"""
+
+from requests import get
+from sys import argv
+
+if __name__ == '__main__':
+    m_url = 'https://jsonplaceholder.typicode.com'
+    t_url = m_url + "/user/{}/todos".format(argv[1])
+    n_url = m_url + "/users/{}".format(argv[1])
+    t_result = get(t_url).json()
+    n_result = get(n_url).json()
+
+    t_num = len(t_result)
+    t_complete = len([todo for todo in t_result
+                         if todo.get("completed")])
+    name = n_result.get("name")
+    print("Employee {} is done with tasks({}/{}):"
+          .format(name, t_complete, t_num))
+    for todo in t_result:
+        if (todo.get("completed")):
+            print("\t {}".format(todo.get("title")))
