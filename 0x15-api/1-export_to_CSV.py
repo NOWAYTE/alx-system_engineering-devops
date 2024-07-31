7b6784e4b38a7f1c770f774217eac0d36a18ed86
NOWAYTE <danielkamaunganga3@gmail.com>
2024-08-01 02:15:13 +0300

    Update

--- 0x15-api/1-export_to_CSV.py
+++ 0x15-api/1-export_to_CSV.py
@@ -19,12 +19,12 @@ if __name__ == "__main__":
     username = response.get("username")
 
     params = {"userId": user}
-    t_response = requests.get(url + "todos",params=params)
+    t_response = requests.get(url + "todos", params=params)
     todos = t_response.json()
 
     with open("{}.csv".format(user), mode="w", newline="") as file:
         writer = csv.writer(file, quoting=csv.QUOTE_ALL)
 
         for i in todos:
-            writer.writerow([user, username, i.get("username"), 
+            writer.writerow([user, username, i.get("username"),
                              i.get("title")])
