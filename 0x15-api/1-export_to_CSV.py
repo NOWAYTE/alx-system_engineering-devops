4b554ac5f77e8da101ac0c065556cadf43a3c944
NOWAYTE <danielkamaunganga3@gmail.com>
2024-08-01 01:03:47 +0300

    Update

--- 0x15-api/1-export_to_CSV.py
+++ 0x15-api/1-export_to_CSV.py
@@ -1,6 +1,7 @@
-#!/usr/bin/python3
+#!/usr/bin/pthon3
 """
-Query data about an employee and save it to a CSV file
+Query data about an employee
+
 """
 
 import csv
@@ -8,46 +9,19 @@ import json
 import sys
 import urllib.request
 
-def fetch(url):
-    """Fetch data from the given URL and return the JSON response."""
-    with urllib.request.urlopen(url) as response:
-        data = json.loads(response.read().decode())
-    return data
-
-def save(user, name, todos):
-    """Save the tasks data to a CSV file."""
-    filename = f"{user}.csv"
-    with open(filename, mode="w", newline='') as file:
-        writer = csv.writer(file, quoting=csv.QUOTE_ALL)
-        writer.writerow(["USER_ID", "USERNAME", "TASK_COMPLETED_STATUS", "TASK_TITLE"])
-        for todo in todos:
-            writer.writerow([user, name, todo.get("completed"), todo.get("title")])
-
-def main(user):
-    main_url = 'https://jsonplaceholder.typicode.com'
-    todo_url = f"{main_url}/users/{user}/todos"
-    name_url = f"{main_url}/users/{user}"
-
-    todos = fetch(todo_url)
-    user_data = fetch(name_url)
-
-    total_tasks = len(todos)
-    completed_tasks = len([todo for todo in todos if todo.get("completed")])
-    username = user_data.get("username")
-    name = user_data.get("name")
-
-    print(f"Employee {name} is done with tasks({completed_tasks}/{total_tasks}):")
-    for todo in todos:
-        if todo.get("completed"):
-            print(f"\t {todo.get('title')}")
-
-    save(user, username, todos)
-
 if __name__ == "__main__":
-    if len(sys.argv) != 2:
-        print("Usage: ./script.py <user_id>")
-        sys.exit(1)
 
-    user_id = sys.argv[1]
-    main(user_id)
+    def fetch(url):
+        with urllib.request.urlopen(url) as response:
+            data = json.loads(url.read().decode())
 
+            return data
+
+    def save(user, name, todo):
+        file = f"{user}.csv"
+
+        with open(file, mode="w") as file:
+            for i in todos:
+                writer.writerow(
+                        [user, name. todo.get("completed"), todo.get("title")]
+                        )
