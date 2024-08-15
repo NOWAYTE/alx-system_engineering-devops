4116c1e776fd16fe1d93ff6c0d8d7f75750a7168
NOWAYTE <danielkamaunganga3@gmail.com>
2024-08-16 00:15:36 +0300

    Code update

--- 0x16-api_advanced/0-subs.py
+++ 0x16-api_advanced/0-subs.py
@@ -6,7 +6,10 @@ Queries Reddit API and returns the number of suscribers
 
 import requests
 
+
 def number_of_subscribers(subreddit):
+    """Returns the number of subscribers"""
+
     url = "https://www.reddit.com/r/{}/about.json".format(subreddit)
 
     headers = {'User-Agent':  'My User Agent'}
@@ -18,4 +21,3 @@ def number_of_subscribers(subreddit):
         return data.get('data').get('subscribers')
     else:
         return 0
-    
