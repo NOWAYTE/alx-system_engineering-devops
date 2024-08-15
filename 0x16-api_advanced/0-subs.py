3c03662ad179503c507c4af307d6534eb5d6451e
NOWAYTE <danielkamaunganga3@gmail.com>
2024-08-16 00:32:53 +0300

    Update

--- 0x16-api_advanced/0-subs.py
+++ 0x16-api_advanced/0-subs.py
@@ -6,13 +6,16 @@ import requests
 
 
 def number_of_subscribers(subreddit):
-    """function that queries the Reddit API and returns the number of
-    subscribers for a given subreddit"""
+    """Returns the number of subscribers"""
+
     url = 'https://www.reddit.com/r/{}/about.json'.format(subreddit)
-    headers = {'User-Agent': 'My User Agent 1.0'}
+
+    headers = {'User-Agent': 'My User Agent'}
     response = requests.get(url, headers=headers)
+
     if response.status_code == 200:
         data = response.json()
         return data.get('data').get('subscribers')
+
     else:
         return 0
