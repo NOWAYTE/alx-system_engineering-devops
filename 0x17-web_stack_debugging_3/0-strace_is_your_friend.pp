99a0ea9859aa3f269432f4a794fdbdc792437224
NOWAYTE <danielkamaunganga3@gmail.com>
2024-08-19 01:48:58 +0300

    Add Comment

--- 0x17-web_stack_debugging_3/0-strace_is_your_friend.pp
+++ 0x17-web_stack_debugging_3/0-strace_is_your_friend.pp
@@ -1,4 +1,6 @@
-exec { 'rename-class-wp-locale-phpp':
+# Fixe apache error
+
+exec { 'Fix typo':
   command => 'mv /var/www/html/wp-includes/class-wp-locale.phpp /var/www/html/wp-includes/class-wp-locale.php',
   onlyif  => 'test -f /var/www/html/wp-includes/class-wp-locale.phpp',
 }
