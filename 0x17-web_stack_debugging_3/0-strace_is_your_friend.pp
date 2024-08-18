b46c97fe515cf525f357ce4479b2d5a221daabb7
NOWAYTE <danielkamaunganga3@gmail.com>
2024-08-19 01:55:01 +0300

    Update

--- 0x17-web_stack_debugging_3/0-strace_is_your_friend.pp
+++ 0x17-web_stack_debugging_3/0-strace_is_your_friend.pp
@@ -1,6 +1,5 @@
 # Fixes Apache 500 error
 
 exec { 'fix typo':
-  command => 'mv /var/www/html/wp-includes/class-wp-locale.phpp /var/www/html/wp-includes/class-wp-locale.php',
-  path    => '/usr/local/bin/:/bin/'
+  command => 'mv /var/www/html/wp-includes/class-wp-locale.phpp /var/www/html/wp-includes/class-wp-locale.php'
 }
